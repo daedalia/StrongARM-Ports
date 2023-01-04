27bc6b364ac06ab7acfb56498ca5d435b3bf3b7e
daedalia <bryn@daedalia.co.uk>
2023-01-04 17:33:49 +0000

    Dandara and Dont Starve - Save location set up

---
+++ Dandara.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+get_controls
+
+$ESUDO chmod 666 /dev/tty1
+printf "\033c" > /dev/tty1
+
+PORTFOLDER=/storage/roms/ports
+GAMEDIR=$PORTFOLDER/dandara
+
+$ESUDO rm -rf ~/.config/unity3d
+$ESUDO mkdir ~/.config/unity3d
+$ESUDO mkdir ~/.config/unity3d/Long\ Hat\ House
+$ESUDO ln -s /$GAMEDIR/savedata ~/.config/unity3d/Long\ Hat\ House/Dandara
+
+export LD_LIBRARY_PATH=$GAMEDIR/lib
+
+cd $GAMEDIR/gamedata/Dandara_Data
+
+$ESUDO chmod 666 /dev/uinput
+
+$GPTOKEYB "Dandara.x86_64" xbox360 &
+
+$PORTFOLDER/MAINLINE/run.sh box64 $GAMEDIR/gamedata/Dandara.x86_64 2>&1 | tee $GAMEDIR/log.txt
+$ESUDO kill -9 $(pidof gptokeyb)
+
+printf "\033c" >> /dev/tty1
---
+++ DontStarve.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+get_controls
+
+PORTFOLDER=/storage/roms/ports
+GAMEDIR=$PORTFOLDER/dontstarve
+
+$ESUDO rm -rf ~/.klei
+$ESUDO mkdir ~/.klei
+$ESUDO ln -s /$GAMEDIR/savedata ~/.klei/DoNotStarve
+
+cd $GAMEDIR/gamedata/bin
+
+$GPTOKEYB "dontstarve" xbox360 -c "$GAMEDIR/control.gptk" &
+$TASKSET $PORTFOLDER/MAINLINE/run.sh box64 $GAMEDIR/gamedata/bin/dontstarve 2>&1 | tee $GAMEDIR/log.txt
+
+$ESUDO kill -9 $(pidof gptokeyb)
+unset SDL_GAMECONTROLLERCONFIG
